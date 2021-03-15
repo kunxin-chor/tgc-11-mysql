65f13207d314511810cf568f187ee6d938462e4e
kunxin-chor <chorkunxin@yahoo.com>
2021-03-15 02:38:39 +0000

    Added more queries

---
+++ queries.sql
@@ -0,0 +1,29 @@
+/* logging in */
+mysql -u root
+
+/* see all databases */
+show databases;
+
+/* create a database */
+create database swimming;
+
+/* switch database */
+use swimming;
+
+/* show all tables */
+show tables;
+
+/* create table */
+create table Parents (
+    parent_id int unsigned auto_increment primary key,
+    surname varchar(50) not null,
+    given_name varchar(50) not null,
+    email varchar(350) not null
+) engine=innodb;
+
+/* insert one row into the table */
+insert into Parents (surname, given_name, email)
+    values ('Phua', 'Chua Kang', 'chukang@phua.com.sg');
+
+/* see all the rows in a table */
+select * from Parents;
