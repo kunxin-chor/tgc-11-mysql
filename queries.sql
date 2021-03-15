cce21e81d080a15090c19367171d8b73b66a4aa6
kunxin-chor <chorkunxin@yahoo.com>
2021-03-15 03:23:46 +0000

    Added table definitions for Venues and Certificates

--- queries.sql
+++ queries.sql
@@ -32,3 +32,27 @@ insert into Parents (surname, given_name, email)
 /* see all the rows in a table */
 select * from Parents;
 
+create table Certificates (
+    certificate_id int unsigned auto_increment primary key,
+    title varchar(100) not null
+) engine=innodb;
+
+insert into Certificates (title) 
+    values ('Basic Swimming'),
+           ('Basic Lifesaving'),
+           ('Advanced Lifesaving');
+
+select * from Certificates;
+
+create table Venues (
+    venue_id tinyint unsigned auto_increment primary key,
+    `name` varchar(100)  not null,
+    `address` varchar(500) not null
+) engine=innodb;
+
+insert into Venues (`name`, address) values
+    ('Yishun Swimming Complex', 'Yishun Ave 4'),
+    ('Ang Mio Ko Swimming Complex', 'Ang Mio Kio Ave 1');
+
+
+describe venues; 
