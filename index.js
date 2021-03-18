58b8272fc9a19120535732656f9b6e637825bcec
kunxin-chor <chorkunxin@yahoo.com>
2021-03-18 06:25:02 +0000

    Added hbs file

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ wax.setLayoutPath("./views/layouts");
 
 app.use(express.urlencoded({ extended: false }));
 
+async function main() {
+    // create a SQL connection
+    const connection = await mysql.createConnection({
+        // host is usually is the web address of the DB or (more commonly) an IP address
+        'host': 'localhost',
+        'user': 'root',
+        'database': 'sakila'
+    });
+
+    // let query = "SELECT * FROM actors";
+    // // the syntax below is known as ARRAY DESTRUCTURING
+    // let [actors] = await connection.execute("SELECT * FROM actor");
+    // // instead of
+    // // let response = await connection.execute("SELECT * FROM actor");
+    // // let actors = response[0];
+    // // for (let a of actors) {
+    // //     console.log(a);
+    // // }
+    // console.log(actors.toJSON())
+
+    app.get('/actors', async (req,res)=>{
+        let [actors] = await connection.execute("SELECT * FROM actor");
+        res.render('actors', {
+            'actors': actors
+        })
+    })
+}
+
+main();
+
 require("handlebars-helpers")({
   handlebars: hbs.handlebars,
 });
